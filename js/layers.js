7ec34e3f9cc8399162c2caf209a43e2f61ffdf3e
FreezaF <123831206+FreezaF@users.noreply.github.com>
2024-04-30 13:50:20 -0400

    m

--- js/layers.js
+++ js/layers.js
@@ -37,7 +37,7 @@ addLayer("d", {
     color: "#4BDC13",
     requires: new Decimal(1), // Can be a function that takes requirement increases into account
     resource: "Plumps Of Ducks", // Name of prestige currency
-    baseResource: "s", // Name of resource prestige is based on
+    baseResource: "Bread", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
