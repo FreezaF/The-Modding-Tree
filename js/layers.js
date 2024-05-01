15a097fa9368589bcbab6533e5a14ad3d2281736
FreezaF <123831206+FreezaF@users.noreply.github.com>
2024-05-01 08:42:13 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -38,7 +38,7 @@ addLayer("d", {
     requires: new Decimal(1), // Can be a function that takes requirement increases into account
     resource: "Plumps Of Ducks", // Name of prestige currency
     baseResource: "Bread", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {return player.s.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
